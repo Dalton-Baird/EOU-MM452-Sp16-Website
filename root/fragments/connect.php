3acd7feecb1c3f2cadbeaf4d83155ef406144d9b
Dalton Baird <dltnbaird@gmail.com>
2016-05-02 17:36:49 -0700

    Moved SQL credentials to a separate file so they can be .gitignore'd

--- root/fragments/connect.php
+++ root/fragments/connect.php
@@ -2,16 +2,14 @@
     /* This file needs to be included in every page that requires access to the MySQL database.
      * It establishes and tests the connection to the MySQL database.
      */
-
-    $server = 'localhost';
-    $username = 'root';
-    $password = '***REMOVED***';
-    $database = 'eou_website';
+     
+     //The MySQL credentials are stored in a separate file
+     require_once $_SERVER['DOCUMENT_ROOT'] . '/fragments/sql-credentials.php';
     
     //Create MySQL Connection
-    $mysql = new mysqli($server, $username, $password, $database);
+    $mysql = new mysqli($mysql_server, $mysql_username, $mysql_password, $mysql_database);
     
     //Check MySQL Connection
     if ($mysql -> connect_error)
-    die('Connection failed: ' . $mysql -> connect_error);
+        die('Connection failed: ' . $mysql -> connect_error);
 ?>
